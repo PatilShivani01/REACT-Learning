97ec99feed07512a4acb82e6a764b0fca2575843
PatilShivani01 <patilshivani2912@gmail.com>
2026-07-23 22:28:01 +0530

    working on notes app

--- NotesApp/NOtes-App/src/App.jsx
+++ NotesApp/NOtes-App/src/App.jsx
@@ -13,8 +13,9 @@ const App = () => {
     console.log("Notes is added by:", title); 
 
     const copyTask = [...task];
-
-    console.log(task)
+    copyTask.push({title, info})
+    setTask(copyTask)
+    
 
     setTitle('')
     setInfo('')
@@ -48,7 +49,7 @@ const App = () => {
             }}
           />
           <button 
-            className='bg-white active:bg-black font-medium w-full text-black px-5 py-2 outline-none rounded'
+            className=' bg-white active:bg-scale-95 font-medium w-full text-black px-5 py-2 outline-none rounded'
           >
             Add Note
           </button>
@@ -56,10 +57,13 @@ const App = () => {
 
       <div className='lg:w-1/2 p-10 lg:border-l-2'>
         <h1 className='text-3xl font-bold'>Recent Notes</h1>
-        <div className='flex flex-wrap gap-5 mt-5 h-full overflow-auto'>
-          <div className='h-40 w-40 rounded-2xl bg-white'></div>
-          <div className='h-40 w-40 rounded-2xl bg-white'></div>
-          <div className='h-40 w-40 rounded-2xl bg-white'></div>
+        <div className='flex flex-wrap items-start justify-start gap-5 mt-5 h-full overflow-auto'>
+           {task.map(function(elem, idx) {
+            return <div key={idx} className="relative h-50 w-40 rounded-xl bg-black py-11 px-4 text-black bg-cover p-4 bg-[url('https://imgs.search.brave.com/b9nc5BMwJ963hopkarex8iyCPZDL5oVweOdhvT2icjI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMjQv/NTg0LzQ2Mi9zbWFs/bC9ibGFuay1zcGFj/ZS13aGl0ZS1zdGlj/a3ktbm90ZS1wbmcu/cG5n')]">
+              <h3 className='leading-tight text-xl font-bold'>{elem.title}</h3>
+              <p className='mt-4 leading-tight font-medium text-gray-500'>{elem.info}</p>
+            </div>
+           })}
         </div>
       </div>
     </div>
